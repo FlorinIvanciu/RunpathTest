1be1ce5ef3c4595215df1c4397245584c9fef109
Florin\Florin Iulian <florin_yulian_91@yahoo.ro>
2018-08-19 14:10:29 +0300

    Create new project

---
+++ RunpathTest/RunpathTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RunpathTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
