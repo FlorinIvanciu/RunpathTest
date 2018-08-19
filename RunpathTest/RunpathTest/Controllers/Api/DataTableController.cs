b3576fa9eb38cd77cd6f59d36c880056d32ba8b6
Florin\Florin Iulian <florin_yulian_91@yahoo.ro>
2018-08-19 18:39:47 +0300

    Add api for returning data

---
+++ RunpathTest/RunpathTest/Controllers/Api/DataTableController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Core;
+using Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace RunpathTest.Controllers.Api
+{
+    [Route("api/[controller]")]
+    public class DataTableController : Controller
+    {
+        private readonly IDataHandler dataHandler;
+
+        public DataTableController(IDataHandler dataHandler)
+        {
+            this.dataHandler = dataHandler;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<DataTableModel>> GetDataAsync()
+        {
+            return await dataHandler.GetDataTableAsync();
+        }
+
+    }
+}
